7b159de5b1d26000e97f0545ff8e88a503c4d371
YassineYounes69 <yassine.younes@esprit.tn>
2021-04-11 14:00:21 +0100

    Initial Commit and push

---
+++ src/main/java/com/example/kidszonea4arctic3/KidsZoneA4arctic3Application.java
@@ -0,0 +1,13 @@
+package com.example.kidszonea4arctic3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidsZoneA4arctic3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KidsZoneA4arctic3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kidszonea4arctic3/KidsZoneA4arctic3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kidszonea4arctic3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidsZoneA4arctic3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
